55d98c350810b96042cb9bda30d00b258893f2db
sam <s.clarkyy.1994@gmail.com>
2025-08-18 20:04:12 +0100

    this is the frist push

---
+++ app.js
@@ -0,0 +1,17 @@
+// Dice Roll Simulator
+
+function rolldice() {
+  const numofdice = document.getElementById("numofdice").value;
+  const diceResult = document.getElementById("diceresult");
+  const diceImages = document.getElementById("diceimages");
+  const values = [];
+  const Images = [];
+
+  for (let i = 0; i < numofdice; i++) {
+    const value = Math.floor(Math.random() * 20) + 1;
+    values.push(value);
+    Images.push(`<img src="dice/${value}.png">`);
+  }
+
+  diceResult.textContent = `dice: ${values.join(", ")}`;
+}
